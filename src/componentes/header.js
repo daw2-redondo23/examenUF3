8481d24e38bf8c9356905ee69c39389e0f2536eb
daw2-redondo23 <redondobarrosoruben@fpllefia.com>
2023-03-09 20:50:24 +0100

    primer commit

---
+++ src/componentes/header.js
@@ -0,0 +1,3 @@
+export const header = {
+    template: `<h4>Esto es el header</h4>`
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import { header } from './componentes/header'
+import { home } from './vistas/home'
+
+document.querySelector('header').innerHTML = header.template
+document.querySelector('main').innerHTML = home.template
